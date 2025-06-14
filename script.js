c2c8573516d1dfe7d80ba43bc8e1dfbdb76d87a5
Lasitha Lokuge <lasithalokuge@gmail.com>
2025-06-14 22:06:22 +0800

    Syncing the code with class code

--- script.js
+++ script.js
@@ -3,6 +3,22 @@
 var userEmail = "shalithadev@gmail.com";
 const userName = "Hashan Shalitha";
 let userAge = 28;
+const isAuthenticated = false; //Boolean value
+const lastName = null; //String value
+const firstName = undefined; //String value
+const userData = {
+    firstName: "Hasitha",
+    lastName: "Shalintha",
+    age: 29,
+};
+// Arrays
+const fruits = ["banana", "apple", "guava"];
+const numbers = [1, 2, 3, 4];
+const users = [
+    {id: 1 , name: "Hashan"},
+    {id: 2 , name: "Shalitha"},
+    {id: 3 , name: "Kavindu"},
+];// Object Array
 
 // userName = "Shalitha Hashan"
 
@@ -17,4 +33,14 @@ trying a comit
 
 userAge = 29;
 
-console.log("I'm second javascript file here!", {userEmail, userName, userAge});
+// console.log("I'm second javascript file here!", {userEmail, userName, userAge});
+
+// console.log(userEmail, typeof userEmail);
+// console.log(userAge, typeof userAge);
+// console.log("value:", isAuthenticated, "type:", typeof isAuthenticated);
+// console.log("value:", lastName, "type:", typeof lastName);
+// console.log("value:", firstName, "type:", typeof firstName);
+// console.log("value:", userData, "type:", typeof userData);
+// console.log("value:", fruits, "type:", typeof fruits);
+// console.log("value:", numbers, "type:", typeof numbers);
+console.log("value:", users, "type:", typeof users);
